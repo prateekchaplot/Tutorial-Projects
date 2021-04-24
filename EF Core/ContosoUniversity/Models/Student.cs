530b0b611edc7dbbed5041cc76e66fe1868e7063
Prateek <prateekchaplot96@gmail.com>
2021-04-24 17:04:12 +0530

    models added for the project

---
+++ EF Core/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
